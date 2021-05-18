24dfe98717a4a0215e8c54cb5d117952b3a4f9bf
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-18 16:12:09 +0100

    Added sssi and settlements to basemap with different coloured polygons

--- DSS.R
+++ DSS.R
@@ -24,3 +24,19 @@ st_crs(sssi)#both 27700
 #transform to lat long
 settlements.ll<-st_transform(settlements, 4326)
 sssi.ll<-st_transform(sssi, 4326)
+plot(sssi.ll)
+
+#add to basemap
+leaflet() %>% 
+  addProviderTiles(providers$Stamen.TerrainBackground) %>%
+  addFeatures(sssi.ll)
+
+leaflet() %>% 
+  addProviderTiles(providers$Stamen.TerrainBackground) %>%
+  addFeatures(settlements.ll)
+
+#trying adding as polygons instead
+leaflet() %>% 
+  addProviderTiles(providers$Stamen.TerrainBackground) %>%
+  addPolygons(data = settlements.ll, color = "red", fillColor = "red", fillOpacity = 0.5) %>%
+  addPolygons(data = sssi.ll, color = "blue", fillColor = "red", fillOpacity = 0.5)
