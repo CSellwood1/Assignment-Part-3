fd574e47cb4f282dd4d327b40bfb7d0d64228033
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-18 14:15:51 +0100

    Prevented upload of GIS data. Loaded in packages and loaded in an elevation basemap

--- DSS.R
+++ DSS.R
@@ -1,1 +1,12 @@
 #Assignment part 3 -DSS
+
+#plan is to make a map of sssi across cumbria
+
+#start with making a map
+options("rgdal_show_exportToProj4_warnings"="none") #suppresses certain warnings
+library(sf)
+library(raster)
+library(mapview)
+
+basemap <- raster("Project Data/elevation1.tif")
+plot(basemap)
