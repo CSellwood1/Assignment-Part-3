d24718c612b3065fcdf055135124e12edd5b6063
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-18 14:52:23 +0100

    Loaded in Stamen.TerrainBackground. Loaded in sssi and settlement shapefiles, and transformed them to lat long

--- DSS.R
+++ DSS.R
@@ -11,3 +11,16 @@ library(leaflet)
 library(leafem)
 
 #going to use a normal background map not an elevation
+leaflet() %>% 
+  addProviderTiles(providers$Stamen.TerrainBackground) #shows terrain with no cities!
+
+#read in shape files
+settlements<- st_read("Project Data/Export_Output.shp")
+sssi<-st_read("Project Data/Export_Output_3.shp")
+#retrieve coord reference system from the objects
+st_crs(settlements)
+st_crs(sssi)#both 27700
+
+#transform to lat long
+settlements.ll<-st_transform(settlements, 4326)
+sssi.ll<-st_transform(sssi, 4326)
