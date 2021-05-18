69a6393325c8dfdfc0144bc003d0b2ba6a132dee
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-18 16:49:43 +0100

    Added a new title

--- Assignment_Part_3/assignment shiny.R
+++ Assignment_Part_3/assignment shiny.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("SSSIs and Settlements in Cumbria"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
